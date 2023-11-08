4c9c60a23e6efaca506b58417e476924aa20e412
tony1693 <toak_16@hotmail.com>
2023-11-08 21:28:36 +0100

    CLASE PERSONA1

---
+++ retospoo/person.js
@@ -0,0 +1,41 @@
+const year = new Date().getFullYear()
+
+class Persona {
+
+    constructor(nombre,altura,peso ){
+        this.nombre= nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.colorPelo = "rubio";
+        this.tonalidad = 'blanco';
+        this.yearOfBirth = year - 1993;
+        this.hobbies = [];
+    }   
+    
+
+    calculoImc() {
+         
+     let resultado = this.peso / (this.altura * this.altura);
+        return resultado
+        
+    }
+    
+
+
+    printAll(){
+        for (let atributos in Persona){
+            let resultado = atributos + "_:" + Persona[atributos];
+            console.log(resultado);
+        }
+    
+        
+    }
+
+    printHobbies(hobbies){
+   this.hobbies.push(hobbies);
+    }  
+    
+}
+let my = new Persona ("tony",1.98,80);
+my.printHobbies("padel , futbol , pc");
+console.log(my);
