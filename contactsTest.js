e13bc70ab46be3ce02317007656a5643858ddf8f
tony1693 <toak_16@hotmail.com>
2023-11-15 16:50:36 +0100

    RETOS 1,2,3

---
+++ contactsTest.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var contacts_1 = require("./contacts");
+var contacto1 = new contacts_1.Contacts();
+var contacto2 = new contacts_1.Contacts();
+var contacto = new contacts_1.Contacts();
+console.log(contacto.printCalendar);
+console.log(contacto1.printCalendar);
+console.log(contacto2.printCalendar);
